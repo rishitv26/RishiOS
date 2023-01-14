0007a0fd1f01b68c6d29942932288eace0c8b238
rishitv26 <varshneyrishit5@gmail.com>
2023-01-13 22:20:59 -0500

    hello poopipants

--- buildscripts/compile.sh
+++ buildscripts/compile.sh
@@ -7,8 +7,8 @@ cd ..
 nasm "bootloader/kernel.asm" -f elf -o "bin/kernel_entry.o"
 nasm "bootloader/zeros.asm" -f bin -o "bin/zero.bin"
 nasm "bootloader/boot.asm" -f bin -o "bin/boot.bin"
-nasm "kernel/interrupts/interrupt.asm" -f bin -o "bin/inter.bin"
-nasm "kernel/interrupts/idt.asm" -f bin -o "bin/idt.bin"
+nasm "kernel/interrupts/interrupt.asm" -f elf -o "bin/inter.o"
+nasm "kernel/interrupts/idt.asm" -f elf -o "bin/int.o"
 
 # linking:
 /usr/local/i386elfgcc/bin/i386-elf-ld -o "bin/full_kernel.bin" -Ttext 0x1000\
@@ -17,8 +17,10 @@ nasm "kernel/interrupts/idt.asm" -f bin -o "bin/idt.bin"
   "bin/common.o"\
   "bin/monitor.o"\
   "bin/idt.o"\
+  "bin/int.o"\
+  "bin/inter.o"\
    --oformat binary
-cat "bin/inter.bin" "bin/idt.bin" "bin/boot.bin" "bin/full_kernel.bin" > "bin/e.bin"
+cat "bin/boot.bin" "bin/full_kernel.bin" > "bin/e.bin"
 cat "bin/e.bin" "bin/zero.bin" > "bin/os.bin"
 
 # end of compilation
