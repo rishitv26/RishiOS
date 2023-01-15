e866a27219234f2a0d1d219d94154fb442192e78
rishitv26 <varshneyrishit5@gmail.com>
2023-01-15 16:36:29 -0500

    reverting

--- kernel/kernel.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-#include "screen/monitor.h"
-
-extern int main ()
-{
-    // boot proccess:
-    print("[*] ", 0, 12); print("Booted into Kernel...\n", 0, 3); // into c entry point
-    return 0; // exit code
-}
--- run_os.sh
+++
@@ -1,8 +0,0 @@
-cd src
-echo IGNORE ANY ERRORS
-make clean  # Ignore errors
-echo DO NOT ANYMORE
-make
-cd ..
-./update_image.sh
-./run_bochs.sh
