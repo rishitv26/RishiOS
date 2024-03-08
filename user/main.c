ae862d8c0431aad7d031c8ade21adec16cafe1b4
rishitv26 <varshneyrishit5@gmail.com>
2024-03-07 20:03:48 -0500

    Having some fun...

--- user/main.c
+++ user/main.c
@@ -3,11 +3,12 @@
 #define DELAY 4000000 // hertz of the cpu
 // basic user program that runs in user mode...
 int main(void) {
-    int counter = 0;
-    while (true) {
-        for (int i = 0; i < 100000; ++i) sleepu(DELAY);
-        printf("proccess1 has %d ticks.\n", counter);
-        counter++;
-    }
+    // int counter = 0;
+    // while (true) {
+    //     for (int i = 0; i < 100000; ++i) sleepu(DELAY);
+    //     printf("proccess1 has %d ticks.\n", counter);
+    //     counter++;
+    // }
+    printf("I wanna do stoga engine\n");
     return 0;
 }
